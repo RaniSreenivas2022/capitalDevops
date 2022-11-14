13311ae42e1c10d1459debcac23ad168fce3f771
dev3 <dev3@mail.com>
2022-11-15 02:09:37 +0530

    modified by dev1

--- hello.java
+++ hello.java
@@ -1,7 +1,5 @@
-class hello   ---> dev1 
-{
+class hello   ---> dev1{
 	public static void main() --> dev1
-	{
-		system.out.println("hello from dev1");  ----> dev1
+	{system.out.println("hello from dev1");  ----> dev1
 	}
 }
